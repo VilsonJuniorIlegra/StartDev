c707f5812151cc89cd7b577c71c545fab4a7df46
VilsonJuniorIlegra <vilson.junior@ilegra.com>
2015-05-13 14:21:13 -0300

    removido métodos e atributos estaticos/static

--- Desafio1/src/br/com/desafio1/calculadora/TesteCalculadora.java
+++ Desafio1/src/br/com/desafio1/calculadora/TesteCalculadora.java
@@ -6,43 +6,29 @@ import org.junit.Test;
 
 public class TesteCalculadora {
 
-	public float valorTeste1 = 4;
-	public float valorTeste2 = 4;
-	public float resultadoEsperado;
-	
 	@Test
 	public void testeSomar(){
-		double resultadoSomaTeste = 8;
-		
-		this.resultadoEsperado = Calculadora.somar(valorTeste1, valorTeste2);
-		
-		assertEquals(resultadoSomaTeste, resultadoEsperado, 0);
+		Calculadora calculadoraTest = new Calculadora();				
+		assertEquals(8, calculadoraTest.somar(4,4), 0);
 	}
 	
 	@Test
 	public void testeSubtrair(){
-		double resultadoSubtraiTeste = 0;
-		
-		this.resultadoEsperado = Calculadora.subtrair(valorTeste1, valorTeste2);
-		assertEquals(resultadoSubtraiTeste, resultadoEsperado, 0);
+		Calculadora calculadoraTest = new Calculadora();				
+		assertEquals(3, calculadoraTest.subtrair(5, 2), 0);
 		
 	}
 	
 	@Test
 	public void testeDividir(){
-		double resultadoDividiTeste = 1;
-		
-		this.resultadoEsperado = Calculadora.dividir(valorTeste1, valorTeste2);
-		assertEquals(resultadoDividiTeste, resultadoEsperado, 0);
-		
+		Calculadora calculadoraTest = new Calculadora();
+		assertEquals(2,	calculadoraTest.dividir(4, 2), 0);
 	}
 	
 	@Test
 	public void testeMultiplicar(){
-		double resultadoMultiplicaTeste = 16;
-		
-		this.resultadoEsperado = Calculadora.multiplicar(valorTeste1, valorTeste2);
-		assertEquals(resultadoMultiplicaTeste, resultadoEsperado, 0);
+		Calculadora calculadoraTest = new Calculadora();
+		assertEquals(10, calculadoraTest.multiplicar(5, 2), 0);
 		
 	}
 
---
+++ Desafio1CodeReview/src/br/com/desafio1codereview/calculdoratest/CalculdoraTest.java
@@ -0,0 +1,14 @@
+package br.com.desafio1codereview.calculdoratest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculdoraTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
