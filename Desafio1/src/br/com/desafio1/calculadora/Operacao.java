2cedcbc6531fd021e3dbc024a6003695b240e025
VilsonJuniorIlegra <vilson.junior@ilegra.com>
2015-05-15 14:49:23 -0300

    refatorando exception

--- Desafio1/src/br/com/desafio1/calculadora/Operacao.java
+++ Desafio1/src/br/com/desafio1/calculadora/Operacao.java
@@ -12,8 +12,8 @@ public class Operacao {
 	}
 
 	public float dividir(float valor1, float valor2) {
-		float resultado = valor1 /valor2;
-		return resultado = (valor2 == 0) ? 0 :resultado;
+		float resultado = valor1 / valor2;
+		return (Float.isInfinite(resultado)? 0 : resultado);
 				
 	}
 
