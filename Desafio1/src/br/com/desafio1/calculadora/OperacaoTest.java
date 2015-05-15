614a3ca6878bc6ddeca165edccba26f4750d8138
VilsonJuniorIlegra <vilson.junior@ilegra.com>
2015-05-15 16:09:17 -0300

    refatorando exception

--- Desafio1/src/br/com/desafio1/calculadora/OperacaoTest.java
+++ Desafio1/src/br/com/desafio1/calculadora/OperacaoTest.java
@@ -2,8 +2,6 @@ package br.com.desafio1.calculadora;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Scanner;
-
 import org.junit.Test;
 
 public class OperacaoTest {
@@ -27,28 +25,11 @@ public class OperacaoTest {
 		assertEquals(2,	calculadoraTest.dividir(4, 2), 0);
 	}
 	
-	@Test(expected = ArithmeticException.class)
-	public void testeDividirPorZero(){
-		int v1 = 2/0;
-		}	
-	
-	
 	@Test
 	public void testeMultiplicar(){
 		Operacao calculadoraTest = new Operacao();
 		assertEquals(10, calculadoraTest.multiplicar(5, 2), 0);
 		
 	}
-	
-	@Test
-	public void testeCapturaDeDadosFuncionaSuccess(){
-		Operacao calculadoraTest = new Operacao();
-		
-	}
-	
-	@Test
-	public void testeCapturaDeDadosFuncionaFailed(){}
-	
-	
 
 }
