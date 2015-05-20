cea0f8e92c5ddba944a3cf655fe88a3e52696f7c
VilsonJuniorIlegra <vilson.junior@ilegra.com>
2015-05-20 17:08:24 -0300

    novo desafio

---
+++ Desafio_2/src/br/com/desafio2/Pessoa.java
@@ -0,0 +1,24 @@
+package br.com.desafio2;
+
+public class Pessoa {
+	String nome;
+	int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	@Override
+	public String toString(){
+		return this.getNome() +" "+ this.getIdade();
+	}
+}
